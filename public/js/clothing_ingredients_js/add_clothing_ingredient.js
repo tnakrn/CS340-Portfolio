2c3d1feab425e7697e0b3379129e2b05ddb3efd0
aldenmchico <aldenmchico@gmail.com>
2023-03-05 01:48:37 -0800

    Created Functionality for Clothing Ingredient and Transaction Details
    Added functionality for the M:N relationships between clothing items and ingredients and transactions and ingredients.

---
+++ public/js/clothing_ingredients_js/add_clothing_ingredient.js
@@ -0,0 +1,67 @@
+// Prevent default action for submitting add transaction
+let addClothingIngredientForm = document.getElementById("add-clothingingredient-form");
+addClothingIngredientForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+});
+
+function addClothingIngredient(clothingID, ingredientID, ingredientQty) {
+    // Put our data we want to send in a javascript object
+    let data = {
+        inputClothingID: clothingID,
+        inputIngredientID: ingredientID,
+        inputIngredientQty: ingredientQty
+    }
+    
+    // Setup our request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-clothing-ingredient-form", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            alert("There was an error with the input.")
+        }
+    }
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+}
+
+// Creates a single row in the transactions table
+function addRowToTable(data) {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("clothingingredients-table");
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRowData = parsedData[parsedData.length - 1]
+
+    // Create a row and cells
+    let clothingIngredientsIDInt = parseInt(newRowData.clothingIngredientsID);
+    let row = document.createElement("TR");
+    row.setAttribute("data-value", `${clothingIngredientsIDInt}`);
+    let idCell = document.createElement("TD");
+    let clothingNameCell = document.createElement("TD");
+    let ingredientNameCell = document.createElement("TD");
+    let ingredientQtyCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    idCell.innerText = newRowData.clothingIngredientsID;
+    clothingNameCell.innerText = newRowData.clothingName;
+    ingredientNameCell.innerText = newRowData.ingredientName;
+    ingredientQtyCell.innerText = newRowData.ingredientQty;
+
+    // Add the cells to the row 
+    row.appendChild(idCell);
+    row.appendChild(clothingNameCell);
+    row.appendChild(ingredientNameCell);
+    row.appendChild(ingredientQtyCell)
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
